1492d3922cf09d4619f006693867e5b124e77231
Harsh Patel <harsh98@hotmail.com>
2019-03-07 22:14:29 -0500

    Organized add such that different addition are just wrapper and users may easily implement their own

--- List/list.c
+++ List/list.c
@@ -5,6 +5,7 @@ struct element
 {
     void *value;
     int type;
+    int length;
 };
 
 struct list
@@ -45,20 +46,46 @@ void destory_list(struct list *oldList)
 
 struct list *add_to_list(struct list *curr, int value)
 {
-    //
+    if (_add(curr, TYPE_INT, &value, sizeof(int), 1))
+        return curr;
+    else
+        return NULL;
+}
+
+struct list *add_to_list(struct list *curr, float value)
+{
+    if (_add(curr, TYPE_FLOAT, &value, sizeof(float), 1))
+        return curr;
+    else
+        return NULL;
+}
+
+struct list *add_to_list(struct list *curr, char *string, int length)
+{
+    if (_add(curr, TYPE_STRING, string, sizeof(char) * length, length))
+        return curr;
+    else
+        return NULL;
+}
+
+struct list *_add(struct list *curr, int value_type, void *value_location, int value_size, int value_length)
+{
+    // if full increase list size
     if (curr->size == curr->capacity)
     {
         _increase_list_size(curr);
     }
     int size = curr->size;
-    // create space of value and add it
-    int *temp_int = (int *)malloc(sizeof(int));
-    if (temp_int)
-        *temp_int = value;
+    // get memory to store value in and copy it
+    void *value = (void *)malloc(value_size);
+    if (value)
+        memcpy(value, value_location, value_size);
     else
         return NULL;
-    curr->elements[size].value = temp_int;
-    curr->elements[size].type = TYPE_INT;
+    // update meta-data
+    curr->elements[size].value = value;
+    curr->elements[size].type = value_type;
+    curr->elements[size].length = value_length;
     curr->size++;
     return curr;
 }
--- List/list.h
+++ List/list.h
@@ -1,11 +1,12 @@
 #ifndef NODE_LIST
 #define NODE_LIST
 
-#define TYPE_CHAR 1;
-#define TYPE_STRING 2;
-#define TYPE_INT 3;
-#define TYPE_FLOAT 4;
-#define TYPE_LONG 5;
-#define TYPE_DOUBLE 6;
+#define TYPE_CHAR 1
+#define TYPE_STRING 2
+#define TYPE_INT 3
+#define TYPE_FLOAT 4
+#define TYPE_LONG 5
+#define TYPE_DOUBLE 6
+#define TYPE_LIST 7
 
 #endif
